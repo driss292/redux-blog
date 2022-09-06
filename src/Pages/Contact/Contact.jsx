e299d51be939634fc8a048d04e5ad5be4c0d0885
driss292 <drisskaci@outlook.fr>
2022-09-06 15:50:56 +0200

    AllRight

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import "./Contact.css ";
 
 export default function Contact() {
-    return <div></div>;
+    return (
+        <div className="container-contact">
+            <h1>Contactez-nous</h1>
+            <p>Par mail : blogreact@gmail.com</p>
+            <p>Par téléphone : XX-XX-XX-XX-XX</p>
+            <p>Rejoingnez-nous sur les réseaux :</p>
+            <ul>
+                <li>Facebook</li>
+                <li>Twitter</li>
+                <li>Youtube</li>
+            </ul>
+        </div>
+    );
 }
